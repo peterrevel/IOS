4a204bdb8d333a8276e21dd192acd63771de1f51
Peter Reveles <peter.revel@gmail.com>
2014-03-27 16:41:24 -0400

    Assignemnt 1 Complete

--- Matchismo/Matchismo/CardGameViewController.m
+++ Matchismo/Matchismo/CardGameViewController.m
@@ -7,23 +7,49 @@
 //
 
 #import "CardGameViewController.h"
+#import "Deck.h"
+#import "PlayingCardDeck.h"
+#import "Card.h"
 
 @interface CardGameViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
+
+@property (strong, nonatomic) Deck *deck;
 
 @end
 
 @implementation CardGameViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+- (Deck *)deck{
+    if (!_deck){
+        _deck = [[PlayingCardDeck alloc] init];
+    }
+    return _deck;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)setFlipCount:(int)flipCount{
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"%d", self.flipCount];
+    NSLog(@"Flipcount = %d", self.flipCount);
 }
+
+- (IBAction)touchCardButton:(UIButton *)sender {
+    if ([sender.currentTitle length]) {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardback.png"]
+                          forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+    } else {
+        Card *card = [self.deck drawRandomCard];
+        if (card){ // if the deck isn't empty, display a new card
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardfront.png"]
+                              forState:UIControlStateNormal];
+            [sender setTitle:card.contents
+                    forState:UIControlStateNormal];
+        }
+    }
+    self.flipCount++;
+}
+
 
 @end
---
+++ Matchismo/Matchismo/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by Peter Reveles on 3/27/14.
+//  Copyright (c) 2014 Peter Reveles. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
