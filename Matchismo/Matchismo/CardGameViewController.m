047cf7ce76fba0253d60b21d2a7e37d81f9e0a8a
Peter Reveles <peter.revel@gmail.com>
2014-04-04 02:49:05 -0400

    Last Move Message now dunctions properly.

--- Matchismo/Matchismo/CardGameViewController.m
+++ Matchismo/Matchismo/CardGameViewController.m
@@ -16,6 +16,10 @@
 @property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *gameModeSegControl;
+@property (weak, nonatomic) IBOutlet UILabel *historyLabel;
+@property (nonatomic, strong) NSString *historyMessage;
+@property (nonatomic) NSInteger scoreOnDisplay;
+@property (nonatomic, strong) Card *previousCard;
 @end
 
 @implementation CardGameViewController
@@ -28,16 +32,23 @@
     return _game;
 }
 
+- (NSString *)historyMessage{
+    return _historyMessage ? _historyMessage : @"";
+}
+
 # pragma mark Functionality Methods
 
 - (IBAction)touchCardButton:(UIButton *)sender {
     int index = (int)[self.cardButtons indexOfObject:sender];
     [self.game chooseCardAtIndex:index];
+    [self loadHistoryMessageForCardAtIndex:index];
     [self updateUIWithSegControlEnabled:NO];
 }
 
 - (IBAction)dealAgain {
     self.game = nil;
+    self.historyMessage = @"";
+    self.scoreOnDisplay = 0;
     [self updateUIWithSegControlEnabled:YES];
 }
 
@@ -67,7 +78,9 @@
         [cardButton setBackgroundImage:[self backgroundImageForCard:card] forState:UIControlStateNormal];
         cardButton.enabled = !card.isMathced;
     }
+    
     self.scoreLabel.text = [NSString stringWithFormat:@"Score: %d", (int)self.game.score];
+    self.historyLabel.text = self.historyMessage;
 }
 
 # pragma mark Back End
@@ -82,6 +95,46 @@
 
 - (UIImage *)backgroundImageForCard:(Card *)card{
     return [UIImage imageNamed:card.isChosen ? @"cardFront" : @"cardBack"];
+}
+
+
+// doesnt work in the following cases:
+// 1. Mismatch
+// 2. Card deselected way back in history
+- (void)loadHistoryMessageForCardAtIndex:(NSInteger)index{
+    // clear out old message if there was a match or mismatch
+    NSRange containsMissMatchMessage = [self.historyMessage rangeOfString:@"don't Match"];
+    NSRange containsMatchMessage = [self.historyMessage rangeOfString:@"Matched"];
+    if (containsMissMatchMessage.length) {
+        // If their was a mismatch, bring up the last card's contents
+        self.historyMessage = [self.previousCard contents];
+    } else if (containsMatchMessage.length) {
+        self.historyMessage = @"";
+    }
+    
+    Card *card = [self.game cardAtIndex:index];
+    NSRange range = [self.historyMessage rangeOfString:[card contents]];
+    if (range.length) {
+        // Card is already in the historyMessage, it must be removed
+        self.historyMessage = [[self.historyMessage substringToIndex:range.location] stringByAppendingString:[self.historyMessage substringFromIndex:(range.location + range.length)]];
+    } else {
+        //Card not already in historyMessage
+        if ([[self.game cardAtIndex:index] isChosen]) {
+            self.historyMessage = [self.historyMessage stringByAppendingString:[[self.game cardAtIndex:index] contents]];
+        }
+    }
+    
+    NSInteger scoreDelta = self.game.score - self.scoreOnDisplay;
+    if (scoreDelta > 0) {
+        // A match was found
+        self.historyMessage = [self.historyMessage stringByAppendingString:[NSString stringWithFormat:@" Matched! (+%ld pts)", scoreDelta]];
+    } else if(scoreDelta < -1) {
+        // A mismatch occured
+        self.historyMessage = [self.historyMessage stringByAppendingString:[NSString stringWithFormat:@" don't Match. (%ld pts)", scoreDelta]];
+    }
+    
+    self.scoreOnDisplay = self.game.score;
+    self.previousCard = card;
 }
 
 @end
